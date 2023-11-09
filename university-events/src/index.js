7a3c2f7baac4e58e148baf0bf44f134628e2e58b
Oleh Zanevych <ozanevych@ancestry.com>
2023-11-09 10:06:29 +0200

    Example of Node.js application

--- university-events/src/index.js
+++ university-events/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {initRouters} = require('./routes');
 
 const protocol = 'http';
 const host = 'localhost';
@@ -6,39 +7,9 @@ const port = 8080;
 
 const app = express();
 
-app.use('/events/:eventId', (req, res, next) => {
-    const {eventId} = req.params;
-    console.log(`Before call endpoint for event with id ${eventId}`);
-    next();
-});
+app.use(express.json());
 
-app.get('/events', (req, res) => {
-    res.send('Get all Events');
-});
-
-app.get('/events/:eventId', (req, res) => {
-    const {eventId} = req.params;
-    res.send(`Get Event with id ${eventId}`);
-});
-
-app.post('/events', (req, res) => {
-    res.status(201).send(`Create Event`);
-});
-
-app.put('/events/:eventId', (req, res) => {
-    const {eventId} = req.params;
-    res.send(`Update Event with id ${eventId}`);
-});
-
-app.patch('/events/:eventId', (req, res) => {
-    const {eventId} = req.params;
-    res.send(`Partially update Event with id ${eventId}`);
-});
-
-app.delete('/events/:eventId', (req, res) => {
-    const {eventId} = req.params;
-    res.send(`Delete Event with id ${eventId}`);
-});
+initRouters(app);
 
 app.listen(port, () => {
     console.log(`App listening at ${protocol}://${host}:${port}`);
---
+++ university-events/src/service/auth.js
@@ -0,0 +1,90 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const userRepository = require('../repository/user');
+
+const jwtSecret = 'test';
+
+const AUTH_TOKEN_HEADER = "Authorization";
+const TOKEN_PREFIX = "Bearer";
+const TOKEN_EXPIRATION_TIME = 15 * 60;
+
+const salt = bcrypt.genSaltSync(10);
+
+const authorizeUser = (req, res, next) => {
+    const authHeader = req.header(AUTH_TOKEN_HEADER);
+
+    if (!authHeader) {
+        return res.status(401).send({errorMessage: 'Missed Authorization header'});
+    }
+
+    const authHeaderParts = authHeader.split(' ');
+    if (authHeaderParts.length != 2 || TOKEN_PREFIX !== authHeaderParts[0]) {
+        return res.status(401).send({errorMessage: 'Invalid format of Auth header!'});
+    }
+
+    const jwtToken = authHeaderParts[1];
+
+    try {
+        jwt.verify(jwtToken, jwtSecret);
+    } catch (e) {
+        res.status(401).send({errorMessage: 'Invalid Auth token!'});
+        return;
+    }
+
+    const {userId, email} = jwt.decode(jwtToken);
+
+    req.userId = userId;
+    req.email = email;
+
+    next();
+}
+
+const signIn = async (req, res) => {
+    const {username, password} = req.body;
+    const user = await userRepository.findAuthDataByUserName(username);
+
+    if (!user) {
+        res.status(401).send({errorMessage: 'Unauthorized'});
+    }
+
+    const isMatch = bcrypt.compareSync(password, user.passwordHash);
+
+    if (isMatch) {
+        const jwtToken = jwt.sign({
+            userId: user.id,
+            email: user.email
+        }, jwtSecret, {
+            expiresIn: TOKEN_EXPIRATION_TIME
+        });
+
+        res.send({token: jwtToken});
+    } else {
+        res.status(401).send({errorMessage: 'Unauthorized'});
+    }
+}
+
+const signUp = async (req, res) => {
+    const user = req.body;
+
+    user.passwordHash = bcrypt.hashSync(user.password, salt);
+
+    try {
+        const [{id}] = await userRepository.createUser(user);
+        user.id = id;
+
+        delete user.password;
+        delete user.passwordHash;
+    } catch (e) {
+        res.status(409).send({errorMessage: 'Username or email is already used!'});
+        return;
+    }
+
+    res.status(201).send(user);
+}
+
+module.exports = {
+    authorizeUser,
+    signIn,
+    signUp
+};
